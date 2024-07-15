4b81b8ce9c5810409819b4267e9f2983f5f718cf
Pranshu <pranshu2.71828@gmail.com>
2024-07-15 21:03:55 +0530

    Update: Simplify to 1 table "logs"

--- src/db.go
+++ src/db.go
@@ -7,19 +7,19 @@ import (
 	"time"
 )
 
-type proj struct {
-	CREATED     time.Time
-	NAME        string
-	DESCRIPTION string
-	REPO_LINK   string
-	ID          uint
-}
+// type proj struct {
+// 	CREATED     time.Time
+// 	NAME        string
+// 	DESCRIPTION string
+// 	REPO_LINK   string
+// 	ID          uint
+// }
 
 type lg struct {
-	CREATED    time.Time
-	LOG_ENTRY  string
-	PROJECT_ID uint
-	ID         uint
+	CREATED   time.Time
+	LOG_ENTRY string
+	PROJECT   string
+	ID        uint
 }
 
 type logDB struct {
@@ -27,19 +27,19 @@ type logDB struct {
 	dataDir string
 }
 
-func (p proj) Name() string {
-	return p.NAME
-}
+// func (p proj) Name() string {
+// 	return p.NAME
+// }
 
-func (p proj) Description() string {
-	return p.DESCRIPTION
-}
+// func (p proj) Description() string {
+// 	return p.DESCRIPTION
+// }
 
 func (l lg) Get() string {
 	return fmt.Sprintf(
-		"%s: {pid %d} %s",
+		"%s: {%s} %s",
 		l.CREATED.Format("2006-01-02"),
-		l.PROJECT_ID,
+		l.PROJECT,
 		l.LOG_ENTRY,
 	)
 }
@@ -53,17 +53,17 @@ func (ldb *logDB) tableExists(name string) bool {
 
 func (ldb *logDB) createTable(name string) error {
 	switch name {
-	case "projects":
-		_, err := ldb.db.Exec(`CREATE TABLE "projects" ( "id" INTEGER, "created" DATETIME, "name" TEXT NOT NULL, "description" TEXT, "repo_link" TEXT, PRIMARY KEY("id" AUTOINCREMENT) )`)
-		return err
+	// case "projects":
+	// 	_, err := ldb.db.Exec(`CREATE TABLE "projects" ( "id" INTEGER, "created" DATETIME, "name" TEXT NOT NULL, "description" TEXT, "repo_link" TEXT, PRIMARY KEY("id" AUTOINCREMENT) )`)
+	// 	return err
 	case "logs":
-		_, err := ldb.db.Exec(`CREATE TABLE "logs" ( "id" INTEGER, "created" DATETIME, "project_id" INTEGER, "log_entry" BLOB, PRIMARY KEY("id" AUTOINCREMENT), FOREIGN KEY(project_id) REFERENCES projects(id) )`)
+		_, err := ldb.db.Exec(`CREATE TABLE "logs" ( "id" INTEGER, "created" DATETIME, "project" TEXT, "log_entry" BLOB, PRIMARY KEY("id" AUTOINCREMENT) )`)
 		return err
 	}
 	return errors.New("The program doesnot support creation of table with the name " + name)
 }
 
 func dummy() {
-	logg := lg{time.Now(), "Log entry", 123, 456}
+	logg := lg{time.Now(), "Log entry", "Dummy Project", 456}
 	fmt.Println(logg.Get())
 }
